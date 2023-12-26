3affb87b9ac4c032d890c6fd5ef7316c23bd3167
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 16:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str: string): string {
+    // Check if the input string is empty
+    if (str.length === 0) {
+        return '';
+    }
+
+    // Find the index of the first space in the string
+    const firstSpaceIndex = str.indexOf(' ');
+
+    // If there is no space, return the entire string
+    if (firstSpaceIndex === -1) {
+        return str;
+    }
+
+    // Return the first word of the string up to the first space
+    return str.substring(0, firstSpaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Examples
+console.log(firstWord('see and stop'));    // Output: 'see'
+console.log(firstWord(' Hello World!'));   // Output: 'Hello'
+console.log(firstWord('12345'));            // Output: '12345'
+console.log(firstWord(''));                 // Output: ''
